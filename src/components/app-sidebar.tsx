29c9d4e32fb03675b243d3f92866862c27f80bd0
Bui Duong <>
2025-03-09 17:05:54 +0700

    cập nhật đổi màu sáng tối

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,7 +3,7 @@ import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
-import Link from 'next/link'; // Sử dụng Link từ next/link
+import Link from 'next/link';
 import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
 import AppLogo from './app-logo';
 
--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 const DarkModeToggle = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -31,9 +32,9 @@ const DarkModeToggle = () => {
   return (
     <button
       onClick={toggleDarkMode}
-      className="fixed top-4 right-4 bg-gray-200 dark:bg-gray-700 text-black dark:text-white p-2 rounded-md"
+      className="fixed top-4 right-4 bg-gray-200 dark:bg-gray-700 text-black dark:text-white p-2 rounded-md flex items-center justify-center"
     >
-      {isDarkMode ? "Light Mode" : "Dark Mode"}
+      {isDarkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
     </button>
   );
 };
